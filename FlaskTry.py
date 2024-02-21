27ae9880739a62c33df490cd7061fbbed2083019
dhineshkumarmech <98449245+dhineshkumarmech@users.noreply.github.com>
2024-02-21 16:56:03 +0530

    Update FlaskTry.py

--- FlaskTry.py
+++ FlaskTry.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world() -> object:
+@app.route("/")
+def hello_world():
     return 'Hii, This is Flask Application'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=2020)
