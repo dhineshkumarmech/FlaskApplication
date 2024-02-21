79fd36edb2a6309d723637d763cbe71bbaeb0f20
dhineshkumarmech <98449245+dhineshkumarmech@users.noreply.github.com>
2024-02-21 17:11:41 +0530

    Update FlaskTry.py

--- FlaskTry.py
+++ FlaskTry.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def hello_world():
     return 'Hii, This is Flask Application'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=2020)
+//if __name__ == '__main__':
+  //  app.run(host='0.0.0.0', port=2020)
