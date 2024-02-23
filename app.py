75ec37d34c74d68a82e0a08a79fe1a5d85cc91a4
dhineshkumarmech <98449245+dhineshkumarmech@users.noreply.github.com>
2024-02-23 14:01:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return 'Hii, This is Flask Application'
-if __name__ == '__main__':
-    app.run()
+
+@app.route('/')
+def index():
+    return {"message": "Hi, This is Flask (Python App)"}
+
+
+app.run()
